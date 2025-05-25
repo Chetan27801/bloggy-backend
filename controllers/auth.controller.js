dda4b418ad35181dd0fbb87c342d1c0f6e8e9167
Chetan <c27801j@gmail.com>
2025-05-25 23:12:23 +0530

    remove password from getUser

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -112,7 +112,7 @@ export const signout = async (req, res) => {
 //get me
 export const getUser = async (req, res) => {
 	try {
-		const user = await User.findById(req.user.id);
+		const user = await User.findById(req.user.id).select("-password");
 		res.status(200).json(user);
 	} catch (error) {
 		res.status(500).json({ message: "Server Error", error: error.message });
